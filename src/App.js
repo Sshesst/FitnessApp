66221cb3bf16caccfdc59416cbdc23b3433a4f51
Sshesst <dimciksalkov1832@gmail.com>
2023-02-18 20:45:17 +0500

    base pattern && structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react'
-import './App.css'
+import { Box } from "@mui/material";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import ExerciseDetail from "./pages/ExerciseDetail";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      App
-    </div>
+    <Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
   );
 }
 
