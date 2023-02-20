acf0a0ffc045608f2a12c51c7190c257738dedc2
Sshesst <dimciksalkov1832@gmail.com>
2023-02-20 14:48:10 +0500

    homepage#3

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -1,23 +1,45 @@
-import { Typography, Box } from '@mui/material'
-import React from 'react'
+import { Typography, Box, Button } from "@mui/material";
+import React from "react";
+import HeroBannerImage from "../assets/images/banner.png"
 
 const HeroBanner = () => {
   return (
-    <Box sx={{
-        mt: { lg: '212px', xs: '70px'},
-        ml: { sm: '50px'}
-    }} position="relative" p="20px">
-        <Typography color='#FF2625' fontWeight="600" fontSize="26px">
-            Fitness club
-        </Typography>
-        <Typography>
-            Sweat, Smile <br /> and Repeat)
-        </Typography>
-        <Typography fontWeight={700}>
-            Check out of the most popular exercises
-        </Typography>
+    <Box
+      sx={{
+        mt: { lg: "212px", xs: "70px" },
+        ml: { sm: "50px" }
+      }}
+      position="relative"
+      p="20px"
+    >
+      <Typography color="#FF2625" fontWeight="600" fontSize="26px">
+        Клуб фитнесса
+      </Typography>
+      <Typography
+        fontWeight={700}
+        sx={{ fontSize: { lg: "44px", xs: "40px" } }}
+        mb={1}
+      >
+        Потей, улыбайся <br /> и не стесняйся)
+      </Typography>
+      <Typography fontSize="22px" lineHeight="35px" mb={4}>
+        Check out of the most popular exercises
+      </Typography>
+      <Button variant="contained" color="error" href="#exercises">
+        Explore Exercise
+      </Button>
+      <Typography fontWeight={600}
+      color="#ff2625"
+      sx={{
+        opacity: 0.1,
+        display: {lg: 'block', xs: 'none'}
+        }}
+        fontSize="200px">
+        Exercise
+      </Typography>
+      <img src={HeroBannerImage} alt="Баннер" className="hero-banner-img"/>
     </Box>
-  )
-}
+  );
+};
 
-export default HeroBanner
+export default HeroBanner;
